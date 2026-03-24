99f49c0ca15158406e58af7152005a44f253efab
Nicolasfreitas6 <nicolasliceski@hotmail.com>
2026-03-23 23:43:54 -0300

    Time: 279 ms (66.81%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,4 +1,6 @@
-# Write your MySQL query statement below
+#--Tipo: Filtro Simples
+#--Conceito: WHERE + OR
+#--Problema: Big Countries (area >= 3000000) or (population >= 25000000)
 SELECT name, population, area
 FROM world
 WHERE area >= 3000000
