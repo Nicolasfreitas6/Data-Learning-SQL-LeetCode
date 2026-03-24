6b082b7af9e06d08ba60bcc60daa0dabcd8e673a
Nicolasfreitas6 <nicolasliceski@hotmail.com>
2026-03-24 00:27:51 -0300

    Time: 1233 ms (49.59%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,4 +1,6 @@
-# Write your MySQL query statement below
+#--Tipo: JOIN (junção de tabelas)
+#--Conceito: LEFT JOIN
+#--Problema: Product Sales Analysis I (product_name, year, price per sale)
 SELECT p.product_name, s.year, s.price
 FROM sales s
 LEFT JOIN product p
