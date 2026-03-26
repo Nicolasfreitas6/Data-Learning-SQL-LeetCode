482e78f8afc8250af1a7d3d131f96a02c89c59d5
Nicolasfreitas6 <nicolasliceski@hotmail.com>
2026-03-25 23:53:00 -0300

    Time: 347 ms (60.63%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,12 @@
+#--Tipo: Agregação + cálculo
+#--Conceito: AVG + CASE + proporção
+#--Problema: Queries Quality and Percentage
+SELECT query_name, 
+    ROUND(AVG(rating / position), 2) AS quality,
+    ROUND(AVG(CASE 
+                WHEN rating < 3 THEN 1
+                ELSE 0
+            END) *100,
+                2) AS poor_query_percentage
+FROM queries
+GROUP BY query_name;
